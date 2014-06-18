b943676f6b33ca064c5f8eadb4c3d51976c9acbd
Toli Carter <drachis@gmail.com>
2014-06-18 16:36:55 -0700

    can find closest value in grey scale 2d data, does not handle image input

---
+++ debug data.py
@@ -0,0 +1,22 @@
+0 = 'thankyo'
+1 = 'uforapp'
+2 = 'lyingfo'
+3 = 'rajobat'
+4 = 'playstu'
+5 = 'dios   '
+
+tulrpdhfyaliaoijaonrnoyskagbsypfatopotu
+
+0 = ['t', 'n', 'u', 'r', 'l', 'n']
+1 = ['o', 'o', 'p', 'i', 'o', 'j']
+2 = ['p', 'y', 'f', 'a', 'a', 'l']
+3 = ['g', 'r', 'b', 'p', 's', 'd']
+4 = ['o', 't', 'y', 'u', 's', 'n']
+5 = ['a', 't', 'o', 'a', 'y', 'o']
+6 = ['i', 'h', 'k', 'f', 'a', 'y']
+7 = ['n', 'u', 'r', 'l', 'n', 'r']
+8 = ['o', 'p', 'i', 'o', 'j', 't']
+9 = ['y', 'f', 'a', 'a', 'l', 't']
+10 = ['r', 'b', 'p', 's', 'd', 'T']
+11 = ['t', 'y', 'u', 's', 'n', 'o']
+12 = ['t', 'o', 'a', 'y', 'o', 'p']
---
+++ test_toli_carter_part_2.py
@@ -0,0 +1,34 @@
+import random
+import math
+
+def findDuplicates(array2d):
+    _dict = {}
+    bestDup = []
+    closest = len(array2d)**2
+    for i,x, in enumerate(array2d):
+        for j,y in enumerate(x):
+            if y not in _dict:
+                _dict[y] = [(i,j)]
+            else:
+                _dict[y].append((i,j))
+    for value in _dict:
+        if len(_dict[value]) > 1:
+            for pair in _dict[value]:
+                _distance = distanceSimple(pair, (0,0))
+                if _distance < closest:
+                    closest  = _distance
+                    bestDup = pair
+    return bestDup
+def distanceSimple(a,b):
+    #distance with out a square root for daster comparisons
+    x = a[0]-b[0]
+    y = a[1]-b[1]
+    return x+y
+
+def distance(a,b):
+    return math.sqrt(istanceSimple(a,b))    
+
+if __name__ == "__main__":
+    dummyData = [[ random.randint(0,258) for x in range(8)] for y in range(8)]
+    dup = findDuplicates(dummyData)
+    print "Closest duplicate value is at: {0},{1}".format(dup[0], dup[1])
