c5a4f3b4da3d51a51a95001b0c720fa44af64f63
Toli Carter <drachis@gmail.com>
2014-06-18 13:13:53 -0700

    crypt and decrypt not working as expected with zip function, returning to manuel transform

--- test_toli_carter_part_1.py
+++ test_toli_carter_part_1.py
@@ -1,3 +1,5 @@
+import math 
+
 def stringToMatrix(_string,_width):
     #break string into a filled matrix
     start = 0
@@ -11,11 +13,30 @@ def stringToMatrix(_string,_width):
         start += _width
     return output
 
+def decrypt(_string,sizeH):
+    sizeV = math.ceil(len(string)/float(_width))
+    v,h= 0    
+    while v < sizeV:
+        pass    
+    return decrypt
+                      
+
 def stringMatrixToCrypt(_list):
     crypt = ""
-    for line in zip(*_list):
+    for line in zip(*_list[::-1]):
         crypt += "".join(line).strip()
     return crypt
+
+def stringMatrixToDecrypt(_list):
+    decrypt = ""
+    for line in zip(*_list)[::-1]:
+        decrypt += "".join(line).strip()
+    return decrypt
+    
+
+def stringToDecrypt(_string, _width):
+    _width = int(math.ceil(len(_string)/float(_width)))
+    return stringMatrixToDecrypt(stringToMatrix(_string, _width))
 
 def stringToCrypt( _string, _width):
     _string = _string.replace(" ","")
@@ -23,10 +44,10 @@ def stringToCrypt( _string, _width):
     
     
 if __name__ == "__main__":
-    _input = "thank you for applying for a job at play studios"
-    #_input = "AAABBBCC"
+    #_input = "thank you for applying for a job at play studios"
+    _input = "1234567890"
     crypt = stringToCrypt(_input,6)
-    uncrypt =  stringToCrypt(crypt,6)
+    uncrypt =  stringToDecrypt(crypt,6)
     pass
 
         
