219c86385da3c548e7136bf666b953725617682c
Toli Carter <drachis@gmail.com>
2014-06-18 12:42:36 -0700

    starting work on decrypt

--- test_toli_carter_part_1.py
+++ test_toli_carter_part_1.py
@@ -1,4 +1,5 @@
-def stringSlice(_string,_width):
+def stringToMatrix(_string,_width):
+    #break string into a filled matrix
     start = 0
     length = len(_string)
     output = []
@@ -10,16 +11,22 @@ def stringSlice(_string,_width):
         start += _width
     return output
 
+def stringMatrixToCrypt(_list):
+    crypt = ""
+    for line in zip(*_list):
+        crypt += "".join(line).strip()
+    return crypt
 
+def stringToCrypt( _string, _width):
+    _string = _string.replace(" ","")
+    return stringMatrixToCrypt(stringToMatrix(_string,_width))
+    
+    
 if __name__ == "__main__":
     _input = "thank you for applying for a job at play studios"
-    #print zip(stringSlice(_input.replace(" ",""),6))
-    
-    _list = zip(*stringSlice(_input.replace(" ",""),6))
-    _crypt = ""
-    for line in _list:
-        _crypt+="".join(line).replace(" ","")
-        
+    #_input = "AAABBBCC"
+    crypt = stringToCrypt(_input,6)
+    uncrypt =  stringToCrypt(crypt,6)
     pass
 
         
