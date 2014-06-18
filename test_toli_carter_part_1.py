8c75cb5b608842d67ada222939bd180ed4bde12b
Toli Carter <drachis@gmail.com>
2014-06-18 12:30:37 -0700

    reduced solution size drasticly, with zip(* method continuing to clean

--- test_toli_carter_part_1.py
+++ test_toli_carter_part_1.py
@@ -1,41 +1,26 @@
 def stringSlice(_string,_width):
     start = 0
     length = len(_string)
+    output = []
     while start < length:
-        yield _string[start:start+_width]
+        filler = 0
+        if length < start+_width:
+            filler = (start+_width) - length
+        output.append(_string[start:start+_width]+filler*" ")
         start += _width
+    return output
 
-def stringToMatrix(_string = None, _width = None):
-    if _input == None or _width == None:
-        print "Not enough input data, requires both string and width"
-        return None
-    _string = _string.replace(" ", "") 
-    stringCrypt = []
-    for line in stringSlice(_string, _width):
-        stringCrypt.append(line)
-    return stringCrypt
-
-def stringMatrixToCryptString(_list):
-    sizeRows = len(_list)
-    sizeColumns = len(_list[0])
-    lastColumn = len(_list[-1])
-    stringCrypt = ""
-    col = 0
-    row = 0
-    while col < sizeColumns:
-        while row < sizeRows:
-            if row != sizeRows-1:
-                stringCrypt += _list[row][col]
-            elif col < lastColumn:
-                stringCrypt += _list[row][col]
-                
-            row += 1
-        col += 1
-        row = 0
-    return stringCrypt
 
 if __name__ == "__main__":
     _input = "thank you for applying for a job at play studios"
-    print zip(*stringToMatrix(_input,6))[0]
+    #print zip(stringSlice(_input.replace(" ",""),6))
+    
+    _list = zip(*stringSlice(_input.replace(" ",""),6))
+    _crypt = ""
+    for line in _list:
+        _crypt+="".join(line).replace(" ","")
+        
     pass
+
+        
         
