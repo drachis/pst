2ad7c23b85533eb9bbca83a30f01914717c87af1
Toli Carter <drachis@gmail.com>
2014-06-18 11:18:48 -0700

    transform to matrix working

--- test_toli_carter_part_1.py
+++ test_toli_carter_part_1.py
@@ -1,2 +1,24 @@
+def stringSlice(_string,_width):
+    start = 0
+    length = len(_string)
+    while start < length:
+        yield _string[start:start+_width]
+        start += _width
 
-print "Hello"
+def stringToMatrix(_string = None, _width = None):
+    if _input == None or _width == None:
+        print "Not enough input data, requires both string and width"
+        return None
+    stringCrypt = []
+    for line in stringSlice(_string.replace(" ", "") , _width):
+        stringCrypt.append(line)
+    return stringCrypt
+        
+
+
+
+if __name__ == "__main__":
+    _input = "This is an input string for testing"
+
+    print stringToMatrix(_input,6)
+        
